b7ab77b1f1ad4733cf91db1e860a485ef430ce73
Samandar <samandarshoyimov04@gmail.com>
2026-07-29 13:52:35 +0200

    test invalid login options at once

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,4 +1,5 @@
 from pages import LoginPage
+import pytest
 
 def test_valid_login(page):
     LoginPage(page).login("standard_user", "secret_sauce")
@@ -8,3 +9,14 @@ def test_invalid_login_shows_error(page):
     lp = LoginPage(page)
     lp.login("standard_user", "wrong_pass")
     assert "do not match" in lp.error_text()
+
+@pytest.mark.parametrize("username,password,expected", [
+    ("bad_user", "secret_sauce", "do not match"),
+    ("standard_user", "wrong_pass", "do not match"),
+    ("", "secret_sauce", "Username is required"),
+    ("standard_user", "", "Password is required"),
+])
+def test_invalid_login(page, username, password, expected):
+    lp = LoginPage(page)
+    lp.login(username, password)
+    assert expected in lp.error_text()
