e0c3cd9ca47e82290ff7a9094fb5e286b7f21b88
Samandar <samandarshoyimov04@gmail.com>
2026-07-29 13:20:42 +0200

    test_login updated according to Class

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,17 +1,10 @@
-def test_valid_login(page):
-    page.goto("https://www.saucedemo.com")
-    page.fill("#user-name", "standard_user")
-    page.fill("#password", "secret_sauce")
-    page.click("#login-button")
+from pages import LoginPage
 
+def test_valid_login(page):
+    LoginPage(page).login("standard_user", "secret_sauce")
     assert "inventory.html" in page.url
 
-
 def test_invalid_login_shows_error(page):
-    page.goto("https://www.saucedemo.com")
-    page.fill("#user-name", "standard_user")
-    page.fill("#password", "wrong_pass")
-    page.click("#login-button")
-
-    error_text = page.locator("[data-test='error']").inner_text()
-    assert "do not match" in error_text
+    lp = LoginPage(page)
+    lp.login("standard_user", "wrong_pass")
+    assert "do not match" in lp.error_text()
